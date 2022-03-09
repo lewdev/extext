be320159dbcb5c96cdfd6173427fe4bbf3ac7c84
Lewis Nakao <lewis.nakao@polarisalpha.com>
2022-03-09 00:15:07 -1000

    Minor changes:
    * add Google Analytics
    * move JS to script.js
    * build.xml adjust compile options

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Fast web development now in your browser using [CodeMirror](https://codemirror.net/).
 
 <p align="center">
-  <img src="https://lewdev.github.io/apps/extext/assets/main.png" width="400"/><br/>
+  <img src="https://lewdev.github.io/apps/extext/assets/main-icon.png" width="400"/><br/>
   <a href="https://lewdev.github.io/apps/extext">👉 Try Extext Here</a><br/>
 </p>
 
@@ -15,19 +15,15 @@ Fast web development now in your browser using [CodeMirror](https://codemirror.n
   <a href="https://lewdev.github.io/apps/extext/screenshots/screenshot-2.png">
     <img src="https://lewdev.github.io/apps/extext/screenshots/screenshot-2.png" width="200" />
   </a>
-  <a href="https://lewdev.github.io/apps/extext/screenshots/screenshot-3.png">
-    <img src="https://lewdev.github.io/apps/extext/screenshots/screenshot-3.png" width="200" />
-  </a>
-  <br/>
-  <a href="https://lewdev.github.io/apps/extext/screenshots/recording-1.gif">
-    <img src="https://lewdev.github.io/apps/extext/screenshots/recording-1.gif" width="200" />
-  </a>
-  <a href="https://lewdev.github.io/apps/extext/screenshots/recording-2.gif">
-    <img src="https://lewdev.github.io/apps/extext/screenshots/recording-2.gif" width="200" />
-  </a>
 </p>
 
 ## 🛠️ Tools & Resources used
+
+* [CodeMirror](https://codemirror.net) the text editor code.
+* html-minifier
+* google-closure-compiler
+* clean-css
+* ant
 
 ## 👤 Author: Lewis Nakao
 I am a software engineer in Hawaii. Find more stuff I made [here](https://lewdev.github.io).
--- src/script.js
+++ src/script.js
@@ -1,120 +1,31 @@
-const main = (() => {
-  const APP_DATA_KEY = "extext";
+var editor = CodeMirror.fromTextArea(c, {
+  autofocus: true,
+  mode: 'htmlmixed',
+  theme: 'material-darker',
+  lineNumbers: true,
+  lineWrapping: true,
+  indentUnit: 2,
+  tabSize: 2,
+  indentWithTabs: false,
+  matchBrackets: true,
+  autoCloseBrackets: true,
+  autoCloseTags: true,
+  matchTags: true,
+  styleActiveLine: true,
+  //for text match-highlighter and show on scrollbar.
+  highlightSelectionMatches: {showToken: /\w/, annotateScrollbar: true},
+});
 
-  const outputDiv = document.getElementById("output");
-  const formOutput = document.getElementById("formOutput");
-  const mainInputs = document.getElementById("mainInputs");
-  const runBtn = document.getElementById("runBtn");
-  const navbar = document.getElementById("navbar");
-  const navbarToggleBtn = document.getElementById("navbarToggleBtn");
+const keyshortcuts = {
+  //run code
+  "Ctrl-Enter": () => f.srcdoc = editor.getValue(),
 
-  let count = 0;
-  let data = {};
+  //indenting
+  "Shift-Tab": "indentLess",
+  "Tab": "indentMore",
 
-  window.onload = () => {
-    loadData();
-    displayData();
+  //toggle comments
+  'Ctrl-/': "toggleComment",
+};
 
-    runBtn.onclick = () => {
-      console.log('ran');
-      count++;
-      getDataByColumn(mainInputs, "dropdown", data);
-      getDataByColumn(mainInputs, "title", data);
-      displayData();
-    };
-
-    const OPEN_CLASS = 'show';
-    const addOffClick = (e, callback) => {
-      const offClick = evt => {
-        if (e !== evt) {
-          callback();
-          document.removeEventListener('click', offClick)
-        }
-      }
-      document.addEventListener('click', offClick)
-    };
-    const handleClick = (e) => {
-      const toggleMenu = () => navbar.classList.toggle(OPEN_CLASS)
-      //e.stopPropagation()
-      if (!navbar.classList.contains(OPEN_CLASS)) {
-        toggleMenu();
-        addOffClick(e, toggleMenu)
-      }
-    };
-    navbarToggleBtn.onclick = handleClick;
-  }
-  const displayData = () => {
-    outputDiv.innerHTML = `Ran ${count} times.`;
-    populateByColumn(formOutput, "dropdown", data);
-    populateByColumn(formOutput, "title", data);
-  }
-  const loadData = () => {
-    const localData = window.localStorage.getItem(APP_DATA_KEY);
-    if (localData) {
-      const parsedData = JSON.parse(localData);
-      if (parsedData) {
-        data = parsedData;
-      }
-    }
-  }
-  const getDataByColumn = (parentElem, colName, obj) => {
-    const input = parentElem.querySelector("." + colName);
-    if (input) {
-      const isCheckbox = input.getAttribute("type") === "checkbox";
-      obj[colName] = isCheckbox ? (input.checked ? 'TRUE' : 'FALSE') : input.value;
-    }
-    else obj[colName] = "";
-  };
-  const populateByColumn = (parentElem, colName, obj, leaveBlank) => {
-    if (!parentElem) return;
-    var elemList = parentElem.querySelectorAll("." + colName);
-    var value = obj[colName];
-    value = value ? value : '';
-    if (elemList) {
-      var i, elem, size = elemList.length;
-      for (i = 0; i < size; i++) {
-        elem = elemList[i];
-        if (elem.tagName === "I") {
-          elem.className = colName + " " + (value === "TRUE" ? "far fa-check-square" : "far fa-square")
-        }
-        else if (elem.tagName === "INPUT" || elem.tagName === "TEXTAREA") {
-          if (elem.getAttribute("type") === "checkbox") {
-            elem.checked = value === "TRUE";
-          }
-          else {
-            elem.value = value;
-          }
-        }
-        else if (elem.tagName === "SELECT") {
-          var options = elem.querySelectorAll("option")
-            , j, optionSize = options.length, selectedIndex = -1;
-          for (j = 0; j < optionSize; j++) {
-            if (value === options[j].value) {
-              options[j].selected = true;
-              selectedIndex = j;
-              break;
-            }
-          }
-          elem.value = value;
-          elem.selectedIndex = selectedIndex;
-          //$(elem).val(value);
-          trigger(elem, "change");
-        }
-        else {
-          if (value && (value + "").trim() !== "") {
-            value = value
-              .replace(/\n/g, '<br/>')
-              .replace(/\s\s/g, '&nbsp;')
-            ;
-          }
-          else {
-            value = leaveBlank ? "" : 'N/A';
-          } 
-          elem.innerHTML = value;
-        }
-      }
-    }
-  };
-  const saveData = () => window.localStorage.setItem(APP_DATA_KEY, JSON.stringify(data));
-  const clearData = () => window.localStorage.setItem(APP_DATA_KEY, JSON.stringify(data));
-})();
+CodeMirror.keyMap.default = Object.assign(CodeMirror.keyMap.default, keyshortcuts);
