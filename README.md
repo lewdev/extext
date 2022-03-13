4a75451411e392a9b16f839ed42c67aa2816fac6
Lewis Nakao <lewdev@gmail.com>
2022-03-12 19:21:37 -1000

    Big update
    * Use sublime text keyshortcuts
    * Save to .html file using Ctrl-S or button
    * Toggle between Vert/Horiz layouts
    * JavaScript hint and autocompolete.

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
 # extext
 Fast web development now in your browser using [CodeMirror](https://codemirror.net/).
 
+I hope the code is not too complex such that you can use the code as the basis to use as a code editor for your own projects.
+
+This project started off from my [tiny-code-editor](https://github.com/lewdev/tiny-code-editor) which was inspired by [Mini Code Editor](https://xem.github.io/miniCodeEditor/) by [xem](https://twitter.com/MaximeEuziere).
+
+## ⚒️ Features
+* Run HTML code in your browser using a Sublime Text-like editor.
+* Download your work to an `html` file (i.e. `extext-20220312-182230.html`)
+* The editor saves what you last ran on `localStorage`.
+* Toggle between Horizontal and Vertical views.
+* Minimal design.
+
+## 💻 CodeMirror Features Enabled
+* Sublime Text key-shortcuts
+* Brackets matching
+* Brackets auto complete
+* Text highlight matching
+* Tag highlight matching
+* JavaScript hint and auto complete. ([details](https://codemirror.net/demo/complete.html))
+
+## ⌨ Additional Key Shortcuts
+* `Ctrl-'` : Run code in `iframe`.
+* `Ctrl-S` : Saves text in editor to `extext-{timestamp}.html` file.
+* `Ctrl-D` : (overridden) Delete line
+* `Shift-Alt-Down` : Duplicate line
+* `Shift-Alt-Up` : Duplicate line
+
 <p align="center">
   <img src="https://lewdev.github.io/apps/extext/img/main-icon.png" width="400"/><br/>
   <a href="https://lewdev.github.io/apps/extext">👉 Try Extext Here</a><br/>
--- src/script.js
+++ src/script.js
@@ -1,7 +1,42 @@
-var editor = CodeMirror.fromTextArea(c, {
+const USER_CONFIG_REF = "extext-config";
+
+// my commands
+const runCode = () => {
+  const content = editor.getValue()
+
+  //save content before running
+  userConfig.content = content;
+  localStorage.setItem(USER_CONFIG_REF, JSON.stringify(userConfig));
+
+  //run code in iframe
+  f.srcdoc = content;
+};
+
+const saveToFile = () => {
+  const content = editor.getValue();
+  const mimeType = "text";
+  const fileName = "extext-" + getTimestamp() + ".html";
+
+  //Code from: https://stackoverflow.com/a/64908345/1675237
+  const a = document.createElement('a') // Create "a" element
+  const blob = new Blob([content], {type: mimeType}) // Create a blob (file-like object)
+  const url = URL.createObjectURL(blob) // Create an object URL from blob
+  a.setAttribute('href', url) // Set "a" element link
+  a.setAttribute('download', fileName) // Set download filename
+  a.click() // Start downloading
+};
+
+const toggleHorizLayout = e => {
+  const { checked } = e.target;
+  userConfig.useHorizLayout = checked;
+  document.body.className = checked ? 'horiz' : '';
+};
+
+const editor = CodeMirror.fromTextArea(c, {
   autofocus: true,
-  mode: 'htmlmixed',
+  mode: {name: "htmlmixed", globalVars: true}, //enables javascript-hint
   theme: 'material-darker',
+  keyMap: "sublime",
   lineNumbers: true,
   lineWrapping: true,
   indentUnit: 2,
@@ -13,19 +48,62 @@ var editor = CodeMirror.fromTextArea(c, {
   matchTags: true,
   styleActiveLine: true,
   //for text match-highlighter and show on scrollbar.
-  highlightSelectionMatches: {showToken: /\w/, annotateScrollbar: true},
+  highlightSelectionMatches: {
+    minChars: 2,
+    showToken: /\w/,
+    annotateScrollbar: true,
+    style:'matchhighlight',
+  },
+  extraKeys: {
+    //run code
+    "Cmd-'": runCode,
+    "Ctrl-'": runCode,
+
+    'Cmd-S': saveToFile,
+    'Ctrl-S': saveToFile,
+
+    'Shift-Alt-Up': "duplicateLine",
+    'Shift-Alt-Down': "duplicateLine",
+
+    "Ctrl-D": "deleteLine",
+    "Cmd-D": "deleteLine",
+
+    "Ctrl-Space": "autocomplete",
+  },
 });
 
-const keyshortcuts = {
-  //run code
-  "Ctrl-Enter": () => f.srcdoc = editor.getValue(),
-
-  //indenting
-  "Shift-Tab": "indentLess",
-  "Tab": "indentMore",
-
-  //toggle comments
-  'Ctrl-/': "toggleComment",
+let userConfig = {
+  useHorizLayout: false,
+  content: "",
 };
 
-CodeMirror.keyMap.default = Object.assign(CodeMirror.keyMap.default, keyshortcuts);
+onload = () => {
+  let storedConfig = localStorage.getItem(USER_CONFIG_REF);
+  if (storedConfig) userConfig = JSON.parse(storedConfig);
+  const { content, useHorizLayout } = userConfig;
+
+  // set the last run content
+  editor.setValue(content);
+
+  if (useHorizLayout) {
+    document.body.className = 'horiz';
+    horiz.checked = true;
+  }
+};
+
+// top bar buttons
+run.onclick = () => runCode();
+save.onclick = () => saveToFile();
+horiz.onclick = toggleHorizLayout;
+
+
+// util methods
+const pad = n => n < 10 ? "0" + n : n;
+
+const getTimestamp = () => {
+  const d = new Date();
+  const dateAttributes = "FullYear, Month, Date, -, Hours, Minutes, Seconds".split(", ");
+  return dateAttributes.map(a => a === '-' ? a : pad(d["get" + a]())).join("");
+};
+
+// console.log(CodeMirror.keyMap.default, CodeMirror.keyMap.sublime);
